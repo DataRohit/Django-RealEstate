193cbf0c950b362028001e72af8e3989be2de946
Rohit Ingole <rohit.vilas.ingole@gmail.com>
2023-10-29 16:11:28 +0530

    Code Refactor

--- realestate/apps/core/views.py
+++ realestate/apps/core/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from realestate.apps.core.models import Couple, House
 
 
 # Create your views here.
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return render(request, "core/homebuyerHome.html", {})
+            couple = Couple.objects.filter(homebuyer__user=request.user)
+            house = House.objects.filter(couple=couple)
+
+            return render(
+                request, "core/homebuyerHome.html", {"couple": couple, "house": house}
+            )
+
         return HttpResponseRedirect(reverse("appauth_login"))
