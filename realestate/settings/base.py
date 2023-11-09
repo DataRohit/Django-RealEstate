e92c436f708a199db0a81f9aa3173f3317ff6731
Rohit Ingole <rohit.vilas.ingole@gmail.com>
2023-11-09 19:36:46 +0530

    Setting Updated

--- realestate/settings/base.py
+++ realestate/settings/base.py
@@ -98,7 +98,9 @@ USE_TZ = True
 
 # Base path for static files
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    (BASE_DIR / "static"),
+]
 STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
 
 
